e54c0550d69d84a97180d2b59543982f5bff7b7b
Sebastian Karlsson <sebastianwyrwik16@gmail.com>
2024-08-27 13:04:42 +0200

    Lavet nogle properties

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,15 @@
+namespace MyFirstGitHubClassRoomConsoleApp60;
+
+public class demo
+{
+    public int Name { get; set; }
+    public DateTime Date { get; set; }
+
+    public demo(int name, DateTime date)
+    {
+        Name = name;
+        Date = date;
+        
+    }
+}
+
